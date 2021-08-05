9e3f15570bccb1b3f4dbc2347f31bfa958c3fcfc
letramsn <letramsn@gmail.com>
2021-08-05 21:14:39 +0700

    add databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,7 +13,15 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    { 
         //\App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('11111111'),
+            'remember_token' => Str::random(10),
+            'role' => 1,
+            'avatar' => '5avatar2.png',
+        ]);
     }
 }
